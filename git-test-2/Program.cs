36fc3983d364839c05ac0321a821b575ae8f02de
Damian Wojtyniak <damian.wojtyniak@globallogic.com>
2021-11-02 10:01:30 +0100

    Initial commit.

---
+++ git-test-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj Damian");
+        }
+    }
+}
